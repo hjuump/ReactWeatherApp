e451473cde1ecf4cfcf75bca38e7a48a36248629
hjuump <164741307+hjuump@users.noreply.github.com>
2024-12-28 09:22:45 +0900

    feat: set up default styles

--- App.tsx
+++ App.tsx
@@ -1,25 +1,69 @@
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View, Dimensions, ScrollView} from 'react-native';
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Weather App</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temperture}>-1</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperture}>-1</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperture}>-1</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperture}>-1</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperture}>-1</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  city: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
   },
-  text: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#000000',
+  cityName: {
+    fontSize: 50,
+    fontWeight: 700,
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temperture: {
+    marginTop: 50,
+    marginBottom: -30,
+    fontSize: 180,
+  },
+  description: {
+    fontSize: 50,
   },
 });
 
